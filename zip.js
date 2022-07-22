b2086c9cd22970df456509341ceb00ee8a55da9e
ericchase <github@eric.addymail.com>
2022-07-22 15:07:38 -0500

    Update zip.js

--- zip.js
+++ zip.js
@@ -15,6 +15,29 @@ function* zip(...iterables) {
     }
 }
 
+/** Potential Pitfalls
+ *
+ *  All iterator protocol methods (next(), return(), and throw()) are expected to return an 
+ *  object implementing the IteratorResult interface. It must have the following properties:
+ * 
+ *  done [Optional]
+ *      A boolean that's false if the iterator was able to produce the next value in the 
+ *      sequence. (This is equivalent to not specifying the done property altogether.)
+ *
+ *      Has the value true if the iterator has completed its sequence. In this case, value 
+ *      optionally specifies the return value of the iterator.
+ *
+ *  value [Optional]
+ *      Any JavaScript value returned by the iterator. Can be omitted when done is true.
+ * 
+ *  In practice, neither property is strictly required; if an object without either property 
+ *  is returned, it's effectively equivalent to { done: false, value: undefined }.
+ * 
+ *  If an iterator returns a result with done: true, any subsequent calls to next() are 
+ *  expected to return done: true as well, although this is not enforced on the language 
+ *  level.
+ */
+
 // Visual Studio Code snippet text
 /*
     "function-zip": {
