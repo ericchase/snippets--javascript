3ea27964019de61931cc06cadab706291bf79023
ericchase <github@eric.addymail.com>
2022-07-22 14:14:08 -0500

    Update zip.js

--- zip.js
+++ zip.js
@@ -14,3 +14,29 @@ function* zip(...iterables) {
         yield values;
     }
 }
+
+// Visual Studio Code snippet text
+/*
+    "function-zip": {
+        "prefix": "function: zip",
+        "body": [
+            "/** Zip Up Iterable Objects",
+            " *",
+            " * @param ...iterables: objects that implement the Symbol.iterator function",
+            " * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols",
+            " */",
+            "function* zip(...iterables) {",
+            "    const iterators = iterables.map(iterable => iterable[Symbol.iterator]());",
+            "    while (true) {",
+            "        let doneCount = 0;",
+            "        const values = iterators",
+            "            .map(iterator => iterator?.next())",
+            "            .map(next => next?.done ? (++doneCount, undefined) : next?.value);",
+            "        if (doneCount === iterators.length) return;",
+            "        yield values;",
+            "    }",
+            "}"
+        ],
+        "description": "function* zip(...iterables) {"
+    },
+*/
