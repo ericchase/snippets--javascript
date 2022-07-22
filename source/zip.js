bba40d338f8ffc91632484b521b317a33d328426
ericchase <about@blank.com>
2022-07-22 17:52:51 -0500

    refactor: move vscode snippet text to .json file

--- source/zip.js
+++ source/zip.js
@@ -67,28 +67,3 @@ function* zip(...iterables) {
  *  expected to return done: true as well, although this is not enforced on the language 
  *  level.
  */
-
-/** Adding to Visual Studio Code snippets
-    "function-zip": {
-        "prefix": "function: zip",
-        "body": [
-            "/** Zip Up Iterable Objects",
-            " *",
-            " * @param ...iterables: objects that implement the Symbol.iterator function",
-            " * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols",
-            " */",
-            "function* zip(...iterables) {",
-            "    const iterators = iterables.map(iterable => iterable[Symbol.iterator]());",
-            "    while (true) {",
-            "        let doneCount = 0;",
-            "        const values = iterators",
-            "            .map(iterator => iterator?.next())",
-            "            .map(next => next?.done ? (++doneCount, undefined) : next?.value);",
-            "        if (doneCount === iterators.length) return;",
-            "        yield values;",
-            "    }",
-            "}"
-        ],
-        "description": "function* zip(...iterables) {"
-    },
-*/
