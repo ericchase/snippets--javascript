104d7a5d1cd720744938ca790455614af0ee7e6b
ericchase <github@eric.addymail.com>
2022-10-05 21:00:24 -0500

    refactor: let to const, deno fmt
    Signed-off-by: ericchase <github@eric.addymail.com>

--- source/range.js
+++ source/range.js
@@ -1,5 +1,5 @@
 /** Create an integer range between [0..<N-1>].
- * 
+ *
  * N is an integer passed to the Array() constructor.
  */
 console.log([...new Array(10).keys()]);
@@ -11,14 +11,14 @@ console.log([...new Array(10).keys()]);
 // The range can be manipulated in various ways using the map method:
 
 // Shifts:
-console.log([...new Array(10).keys()].map(i => i + 1));
+console.log([...new Array(10).keys()].map((i) => i + 1));
 // [
 //     1, 2, 3, 4,  5,
 //     6, 7, 8, 9, 10
 // ]
 
 // Multiples:
-console.log([...new Array(10).keys()].map(i => i * 2));
+console.log([...new Array(10).keys()].map((i) => i * 2));
 // [
 //      0,  2,  4,  6,  8,
 //     10, 12, 14, 16, 18
@@ -27,9 +27,9 @@ console.log([...new Array(10).keys()].map(i => i * 2));
 // The filter method can be used for similar effects:
 
 // Evens:
-console.log([...new Array(10).keys()].filter(i => i % 2 === 0));
+console.log([...new Array(10).keys()].filter((i) => i % 2 === 0));
 // [ 0, 2, 4, 6, 8 ]
 
 // Odds:
-console.log([...new Array(10).keys()].filter(i => i % 2 === 1));
+console.log([...new Array(10).keys()].filter((i) => i % 2 === 1));
 // [ 1, 3, 5, 7, 9 ]
--- source/zip.js
+++ source/zip.js
@@ -6,26 +6,27 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols
  */
 function* safeZip(...iterables) {
-    function process(iterators, fakerable) {
-        const values = [];
-        for (let index = 0; index < iterators.length; ++index) {
-            const next = iterators[index]?.next?.();
-            if (next && next?.done === true) {
-                iterators.splice(index, 1, fakerable);
-                ++fakerable.count;
-            }
-            values.push(next?.value);
-        }
-        return values;
+  function process(iterators, fakerable) {
+    const values = [];
+    for (let index = 0; index < iterators.length; ++index) {
+      const next = iterators[index]?.next?.();
+      if (next && next?.done === true) {
+        iterators.splice(index, 1, fakerable);
+        ++fakerable.count;
+      }
+      values.push(next?.value);
     }
-    const fakerable = { count: 0, next: () => ({}) };
-    const iterators = iterables.map((iterable) =>
-        iterable?.[Symbol.iterator]?.() ?? (++fakerable.count, fakerable));
-    let values = process(iterators, fakerable);
-    while (fakerable.count < iterators.length) {
-        yield values;
-        values = process(iterators, fakerable);
-    }
+    return values;
+  }
+  const fakerable = { count: 0, next: () => ({}) };
+  const iterators = iterables.map((iterable) =>
+    iterable?.[Symbol.iterator]?.() ?? (++fakerable.count, fakerable)
+  );
+  let values = process(iterators, fakerable);
+  while (fakerable.count < iterators.length) {
+    yield values;
+    values = process(iterators, fakerable);
+  }
 }
 
 /** Zip Up Iterable Objects
@@ -34,36 +35,36 @@ function* safeZip(...iterables) {
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols
  */
 function* zip(...iterables) {
-    const iterators = iterables.map((iterable) => iterable[Symbol.iterator]());
-    while (true) {
-        let doneCount = 0;
-        const values = iterators
-            .map((iterator) => iterator?.next())
-            .map((next) => (next?.done ? (++doneCount, undefined) : next?.value));
-        if (doneCount === iterators.length) return;
-        yield values;
-    }
+  const iterators = iterables.map((iterable) => iterable[Symbol.iterator]());
+  while (true) {
+    let doneCount = 0;
+    const values = iterators
+      .map((iterator) => iterator?.next())
+      .map((next) => (next?.done ? (++doneCount, undefined) : next?.value));
+    if (doneCount === iterators.length) return;
+    yield values;
+  }
 }
 
 /** Potential Pitfalls with unsafe Zip
  *
- *  All iterator protocol methods (next(), return(), and throw()) are expected to return an 
+ *  All iterator protocol methods (next(), return(), and throw()) are expected to return an
  *  object implementing the IteratorResult interface. It must have the following properties:
- * 
+ *
  *  done [Optional]
- *      A boolean that's false if the iterator was able to produce the next value in the 
+ *      A boolean that's false if the iterator was able to produce the next value in the
  *      sequence. (This is equivalent to not specifying the done property altogether.)
  *
- *      Has the value true if the iterator has completed its sequence. In this case, value 
+ *      Has the value true if the iterator has completed its sequence. In this case, value
  *      optionally specifies the return value of the iterator.
  *
  *  value [Optional]
  *      Any JavaScript value returned by the iterator. Can be omitted when done is true.
- * 
- *  In practice, neither property is strictly required; if an object without either property 
+ *
+ *  In practice, neither property is strictly required; if an object without either property
  *  is returned, it's effectively equivalent to { done: false, value: undefined }.
- * 
- *  If an iterator returns a result with done: true, any subsequent calls to next() are 
- *  expected to return done: true as well, although this is not enforced on the language 
+ *
+ *  If an iterator returns a result with done: true, any subsequent calls to next() are
+ *  expected to return done: true as well, although this is not enforced on the language
  *  level.
  */
