8668056c3e86e8df78a5a68784fd03e7aba8eb90
ericchase <github@eric.addymail.com>
2022-10-22 20:16:01 -0500

    fix: some mistakes, removed redundant code

--- source/cartesian-product.js
+++ source/cartesian-product.js
@@ -11,30 +11,41 @@ function getCartesianProduct(arr1, arr2) {
     return [...generateCartesianProduct(arr1, arr2)];
 }
 
-function flattenedCartesianProduct(arr1, arr2) {
-    return getCartesianProduct(arr1, arr2)
-        .map(a => a.flat(1));
-}
-
-function consecutiveFlatCartesianProduct(parts) {
+function getConsecutiveCartesianProduct(parts) {
     return parts.reduce((product, current) =>
-        flattenedCartesianProduct(product, current), [[]]);
+        getCartesianProduct(product, current)
+            .map(a => a.flat(1))
+        , [[]])
 }
 
-// Simple cartesian product of two arrays:
-console.log(getCartesianProduct("a", ["b", "c"]));
+console.log('Cartesian Products');
+
+// Simple cartesian product of two items:
+console.log('\n2 Items:');
+console.log(getCartesianProduct('a', 'b'));
+console.log(getCartesianProduct(['a'], ['b']));
+// [
+//     ['a', 'b']
+// ]
+console.log(getCartesianProduct('a', ['b', 'c']));
+console.log(getCartesianProduct(['a'], ['b', 'c']));
 // [
 //     ['a', 'b'],
 //     ['a', 'c']
 // ]
 
-// Simple cartesian product of three arrays:
-console.log(consecutiveFlatCartesianProduct(['a', ['b', 'c'], 'd']));
+
+console.log('\nConsecutive Cartesian Products');
+
+// Consecutive cartesian product of three items:
+console.log('\n3 Items:');
+console.log(getConsecutiveCartesianProduct(['a', ['b', 'c'], 'd']));
+console.log(getConsecutiveCartesianProduct([['a'], ['b', 'c'], ['d']]));
 // [
 //     ['a', 'b', 'd'],
 //     ['a', 'c', 'd']
 // ]
-console.log(consecutiveFlatCartesianProduct(['a', ['b', 'c'], ['d', 'e']]));
+console.log(getConsecutiveCartesianProduct(['a', ['b', 'c'], ['d', 'e']]));
 // [
 //     ['a', 'b', 'd'],
 //     ['a', 'b', 'e'],
@@ -42,8 +53,9 @@ console.log(consecutiveFlatCartesianProduct(['a', ['b', 'c'], ['d', 'e']]));
 //     ['a', 'c', 'e']
 // ]
 
-// Consecutive cartesian products of four arrays:
-console.log(consecutiveFlatCartesianProduct(["a", ["b", "c"], "d", ["e", "f", "g"]]));
+// Consecutive cartesian products of four items:
+console.log('\n4 Items:');
+console.log(getConsecutiveCartesianProduct(['a', ['b', 'c'], 'd', ['e', 'f', 'g']]));
 // [
 //     ['a', 'b', 'd', 'e'],
 //     ['a', 'b', 'd', 'f'],
@@ -62,19 +74,79 @@ function* generateSelfCartesianProduct(arr1) {
     }
 }
 
-function getSelfCartesianProduct(arr1, arr2) {
-    return [...generateSelfCartesianProduct(arr1, arr2)]
-        .map(a => a.flat(1));
+function getSelfCartesianProduct(arr1) {
+    return [...generateSelfCartesianProduct(arr1)];
 }
 
-// Simple self cartesian product:
-console.log(getSelfCartesianProduct(["e", "f", "g"]));
+console.log('\n\nSelf Cartesian Products');
+
+// Self cartesian product of 2 items:
+console.log('\n2 Items:');
+console.log(getSelfCartesianProduct(['a', 'b']));
 // [
-//     ['e', 'f'],
-//     ['e', 'g'],
-//     ['f', 'g']
+//     ['a', 'b']
+// ]
+console.log(getSelfCartesianProduct(['a', ['b', 'c']]));
+//[
+//    ['a', ['b', 'c']]
+//]
+console.log(getSelfCartesianProduct([['a', 'b'], 'c']));
+//[
+//    [['a', 'b'], 'c']
+//]
+console.log(getSelfCartesianProduct([['a', 'b'], ['c', 'd']]));
+//[
+//    [['a', 'b'], ['c', 'd']]
+//]
+
+// Self cartesian product of 3 items:
+console.log('\n3 Items:');
+console.log(getSelfCartesianProduct(['a', 'b', 'c']));
+// [
+//     ['a', 'b'],
+//     ['a', 'c'],
+//     ['b', 'c']
+// ]
+console.log(getSelfCartesianProduct([['a', 'b'], ['c', 'd'], ['e', 'f']]));
+// [
+//     [['a', 'b'], ['c', 'd']],
+//     [['a', 'b'], ['e', 'f']],
+//     [['c', 'd'], ['e', 'f']]
 // ]
 
-// Self cartesian product only works on single depth arrays, like vectors:
-console.log(getSelfCartesianProduct("a", ["b", "c"]));
-// []
+// Self cartesian product of 4 items:
+console.log('\n4 Items:');
+console.log(getSelfCartesianProduct(['a', 'b', 'c', 'd']));
+// [
+//     ['a', 'b'],
+//     ['a', 'c'],
+//     ['a', 'd'],
+//     ['b', 'c'],
+//     ['b', 'd'],
+//     ['c', 'd']
+// ]
+console.log(getSelfCartesianProduct([['a', 'b'], ['c', 'd'], ['e', 'f'], ['g', 'h']]));
+// [
+//     [['a', 'b'], ['c', 'd']],
+//     [['a', 'b'], ['e', 'f']],
+//     [['a', 'b'], ['g', 'h']],
+//     [['c', 'd'], ['e', 'f']],
+//     [['c', 'd'], ['g', 'h']],
+//     [['e', 'f'], ['g', 'h']]
+// ]
+
+// Self cartesian product of complex items:
+console.log('\n5 Complex Items:');
+console.log(getSelfCartesianProduct(['a', ['b', 'c'], 'd', ['e', 'f', 'g'], ['h']]));
+// [
+//     ['a', ['b', 'c']],
+//     ['a', 'd'],
+//     ['a', ['e', 'f', 'g']],
+//     ['a', ['h']],
+//     [['b', 'c'], 'd'],
+//     [['b', 'c'], ['e', 'f', 'g']],
+//     [['b', 'c'], ['h']],
+//     ['d', ['e', 'f', 'g']],
+//     ['d', ['h']],
+//     [['e', 'f', 'g'], ['h']]
+// ]
